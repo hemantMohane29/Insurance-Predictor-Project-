61148f11a17e0209c6522579496752f02e993585
Hemant Mohane <hemantmohane29@gmail.com>
2026-02-24 01:17:06 +0530

    Fix gunicorn configuration for Render

--- insurance/settings.py
+++ insurance/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-hnobo%1^eqj1t4r&6tddq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*'] if DEBUG else os.environ.get('ALLOWED_HOSTS', '.onrender.com').split(',')
 
 
 # Application definition
